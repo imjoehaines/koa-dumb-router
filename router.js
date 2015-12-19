ae847011826f1bdbcdbaa24ad78ed404483d30b3
Joe Haines <hello@joehaines.co.uk>
2015-12-19 22:25:40 +0000

    Tests for url matching

--- router.js
+++ router.js
@@ -1,16 +1,6 @@
 'use strict'
 
-function urlMatches (url, route) {
-  const routeSections = route.split('/')
-  const urlSections = url.split('/')
-
-  if (routeSections.length !== urlSections.length) return false
-
-  // check every section of the route url is a placeholder or matches the url
-  return routeSections.every((section, index) => {
-    return section.startsWith(':') || section === urlSections[index]
-  })
-}
+const urlMatches = require('./lib/urlMatches')
 
 function getArguments (url, route) {
   const routeSections = route.split('/')
@@ -26,6 +16,7 @@ function getArguments (url, route) {
 
 module.exports = function (route, handler) {
   return function * (next) {
+    // if we can't handle the request, continue to the next function
     if (!urlMatches(this.url, route)) return yield * next
 
     const args = getArguments(this.url, route)
---
+++ test/urlMatchesTest.js
@@ -0,0 +1,55 @@
+'use strict'
+
+import test from 'ava'
+
+const urlMatches = require('../lib/urlMatches')
+const Chance = require('chance')
+const chance = new Chance()
+
+test('it matches the root url', t => {
+  t.true(urlMatches('/', '/'))
+})
+
+test('it matches two identical urls', t => {
+  // make a random url and remove http://
+  let url = chance.url({domain: 'abc'})
+  url = url.replace('http://', '')
+
+  t.true(urlMatches(url, url))
+})
+
+test(`it doesn't match two different urls`, t => {
+  let url = chance.url({domain: 'abc'})
+  url = url.replace('http://', '')
+
+  let route = chance.url({domain: 'abc'})
+  route = route.replace('http://', '')
+
+  t.false(urlMatches(url, route))
+})
+
+test(`it doesn't match different urls with the same length`, t => {
+  const count = chance.integer({min: 2, max: 30})
+  const url = chance.n(chance.word, count).join('/')
+  const route = chance.n(chance.word, count).join('/')
+
+  t.false(urlMatches(url, route))
+})
+
+test('it matches urls with placeholders', t => {
+  const url = '/users/12/cats/31'
+  const route = '/users/:userId/cats/:catId'
+
+  t.true(urlMatches(url, route))
+})
+
+test('it matches random urls with placeholders', t => {
+  const count = chance.integer({min: 2, max: 30})
+  const url = chance.n(chance.word, count)
+
+  const route = url.map((word, index) => {
+    return Math.round(Math.random()) === 1 && word || ':placeholder'
+  })
+
+  t.true(urlMatches(url.join('/'), route.join('/')))
+})
